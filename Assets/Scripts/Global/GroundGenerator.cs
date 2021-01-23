6d318133ad4688d6cf149f06f5fff9c46cb14d1e
justinfernald <jfernald@my.gcu.edu>
2021-01-23 16:25:33 -0700

    infinite workspace work

--- Assets/Scripts/Global/GroundGenerator.cs
+++ Assets/Scripts/Global/GroundGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GroundGenerator : MonoBehaviour
 {
-    public static List<GroundGenerator> placements;
+    public static HashSet<Vector2> placements;
     public Vector2 groundSize;
     public Vector2 location;
 
@@ -23,5 +23,17 @@ public class GroundGenerator : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("bruh moment");
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue;
+                if (placements.Contains(location + new Vector2(i, j))) continue;
+                placements.Add(location + new Vector2(i, j));
+                GameObject nGO = Instantiate(gameObject);
+                nGO.transform.position += new Vector3(groundSize.x * i, groundSize.y * j, 0);
+            }
+        }
+        
     }
 }
